1b4c0588da539378dcb22d4208c117698ec1b1ee
Scoutzknifez <Scoutzknifez@gmail.com>
2022-05-01 00:52:16 -0700

    Did some changes to camera controller

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -13,10 +13,17 @@ public class CameraController : MonoBehaviour
     [Range(.1f, 5f)]
     private float degreestoRotatePerFrame;
 
+    Quaternion initRot;
+
+    private void Start()
+    {
+        initRot = transform.rotation;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        transform.position = ball.transform.position + cameraOffset;
+        updateLocation();
 
         bool qPressed = Input.GetKey("q");
         bool ePressed = Input.GetKey("e");
@@ -35,7 +42,14 @@ public class CameraController : MonoBehaviour
     {
         cameraOffset = transform.position - ball.transform.position;
         cameraOffset = Quaternion.Euler(0, isPositive ? degreestoRotatePerFrame : -degreestoRotatePerFrame, 0) * cameraOffset;
+        updateLocation();
+        Vector3 ballPos = ball.transform.position;
+        Vector3 cameraLookVector = new Vector3(ballPos.x, ballPos.y + .435f, ballPos.z);
+        transform.LookAt(cameraLookVector);
+    }
+
+    void updateLocation()
+    {
         transform.position = cameraOffset + ball.transform.position;
-        transform.LookAt(ball.transform.position);
     }
 }
