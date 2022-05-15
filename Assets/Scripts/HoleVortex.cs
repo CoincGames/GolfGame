d6c54b987288c6446a36bedb72401531ee0db027
Scoutzknifez <Scoutzknifez@gmail.com>
2022-05-14 17:15:37 -0700

    Added hole vortex

---
+++ Assets/Scripts/HoleVortex.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class HoleVortex : MonoBehaviour
+{
+    private Collider vortexCollider;
+
+    [SerializeField]
+    private float vortexForce = .5f;
+
+    private void Awake()
+    {
+        vortexCollider = GetComponent<Collider>();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Vector3 normal = other.transform.position - vortexCollider.bounds.center;
+            other.attachedRigidbody.AddForce(normal * vortexForce);
+        }
+    }
+}
