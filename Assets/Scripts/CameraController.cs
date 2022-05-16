d8488e7cb6ff494b8df9c1ae1886475e77c3459c
ConnorWolanski <ronnocool222@gmail.com>
2022-05-15 20:12:18 -0700

    Initial camera control concept

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,58 +3,104 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField]
-    private Vector3 cameraOffset = new Vector3(-1.2f, 1f, 0);
-
-    [SerializeField]
     [Range(0.1f, 10f)]
-    private float rotationSensitivity = .5f;
+    private float mouseSensitivity = 1f;
+
+    public GameObject cameraCenter;
+    public Camera cam;
+    public float scrollSensitivity = 5f;
+    public float scrollDampening = 6f;
+    public float zoomMin = 0f;
+    public float zoomMax = 15f;
+    public float zoomDefault = .14f;
+    public float zoomDistance;
+    
+    public float yOffset = .435f;	
+    private RaycastHit _camHit;
+	// This one is public but no need to input any values for it
+    public Vector3 camDist;
 
     [SerializeField]
     private GameObject golfballGroup;
 
+    public float collisionSensitivity = 1f;
+    // Camera Target
     private GameObject ball;
     private DirectionIndicator directionArrowController;
-
-    private float degreestoRotatePerFrame = .5f;
 
     private void Awake()
     {
         ball = golfballGroup.GetComponentInChildren<Collider>().gameObject;
-        directionArrowController = golfballGroup.GetComponentInChildren<DirectionIndicator>();
+        camDist = cam.transform.localPosition;
+        zoomDistance = zoomDefault;
+        camDist.z = zoomDistance;
+        transform.LookAt(ball.transform.position);
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!Input.GetMouseButton(0))
+        {
+            rotate();
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") != 0f)
+        {
+            zoom();
+        }
         updateLocation();
-
-        bool qPressed = Input.GetKey("q");
-        bool ePressed = Input.GetKey("e");
-
-        if (qPressed)
-        {
-            rotate(false);
-        }
-        else if (ePressed)
-        {
-            rotate(true);
-        }
     }
 
-    void rotate(bool isPositive)
+    void rotate()
     {
-        cameraOffset = transform.position - ball.transform.position;
-        float rotationDegree = (isPositive ? degreestoRotatePerFrame : -degreestoRotatePerFrame) * rotationSensitivity;
-        cameraOffset = Quaternion.Euler(0, rotationDegree, 0) * cameraOffset;
-        directionArrowController.rotate(rotationDegree);
-        updateLocation();
+        var position1 = ball.transform.position;
+        cameraCenter.transform.position = new Vector3(position1.x, position1.y + yOffset, position1.z);
+
+        var rotation = cameraCenter.transform.rotation;
+        rotation = Quaternion.Euler(rotation.eulerAngles.x - Input.GetAxis("Mouse Y") * mouseSensitivity, rotation.eulerAngles.y + Input.GetAxis("Mouse X") * mouseSensitivity, rotation.eulerAngles.z);
+        cameraCenter.transform.rotation = rotation;
+
+    }
+    void zoom()
+    {
+        var scrollAmount = Input.GetAxis("Mouse ScrollWheel") * scrollSensitivity;
+        scrollAmount *= (zoomDistance * 0.3f);
+        zoomDistance += scrollAmount * -1f;
+        zoomDistance = Mathf.Clamp(zoomDistance, zoomMin, zoomMax);
     }
 
     void updateLocation()
     {
-        transform.position = cameraOffset + ball.transform.position;
-        Vector3 ballPos = ball.transform.position;
-        Vector3 cameraLookVector = new Vector3(ballPos.x, ballPos.y + .435f, ballPos.z);
-        transform.LookAt(cameraLookVector);
+        if (camDist.z != zoomDistance * -1f)
+        {
+	        camDist.z = Mathf.Lerp(camDist.z, -zoomDistance, Time.deltaTime * scrollDampening);
+        }
+        var tempTransform = cam.transform;
+        tempTransform.localPosition = camDist;
+
+        // Check and handle Collision
+        GameObject obj = new GameObject();
+        obj.transform.SetParent(tempTransform.parent);
+        var position = cam.transform.localPosition;
+        obj.transform.localPosition = new Vector3(position.x, position.y, position.z - collisionSensitivity);
+		/*
+		Linecast is an alternative to Raycast, using it to cast a ray between the CameraCenter 
+		and a point directly behind the camera (to smooth things, that's why there's an "obj"
+		GameObject, that is directly behind cam)
+		*/
+        if(Physics.Linecast(cameraCenter.transform.position, obj.transform.position, out _camHit))
+        {
+			//This gets executed if there's any collider in the way
+			var transform1 = cam.transform;
+			transform1.position = _camHit.point;
+            var localPosition = transform1.localPosition;
+            localPosition = new Vector3(localPosition.x, localPosition.y, localPosition.z + collisionSensitivity);
+            transform1.localPosition = localPosition;
+        }
+		// Clean up
+        Destroy(obj);
+        transform.LookAt(ball.transform.position);
     }
 }
